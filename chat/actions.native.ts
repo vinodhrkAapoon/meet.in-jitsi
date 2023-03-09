24db12d3c73098e93c2a50fc53e7058de710bb7c
vinodhrkAapoon <124334809+vinodhrkAapoon@users.noreply.github.com>
2023-03-09 17:37:21 +0530

    Add files via upload

---
+++ chat/actions.native.ts
@@ -0,0 +1,34 @@
+/* eslint-disable lines-around-comment, max-len */
+
+import { navigate }
+// @ts-ignore
+    from '../mobile/navigation/components/conference/ConferenceNavigationContainerRef';
+// @ts-ignore
+import { screen } from '../mobile/navigation/routes';
+
+import { OPEN_CHAT } from './actionTypes';
+
+export * from './actions.any';
+
+/**
+ * Displays the chat panel.
+ *
+ * @param {Object} participant - The recipient for the private chat.
+ * @param {boolean} disablePolls - Checks if polls are disabled.
+ *
+ * @returns {{
+ *     participant: participant,
+ *     type: OPEN_CHAT
+ * }}
+ */
+export function openChat(participant: Object, disablePolls: boolean) {
+    if (disablePolls) {
+        navigate(screen.conference.chat);
+    }
+    navigate(screen.conference.chatandpolls.main);
+
+    return {
+        participant,
+        type: OPEN_CHAT
+    };
+}
